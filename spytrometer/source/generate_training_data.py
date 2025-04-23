ae63ac27bc30889ad2e2beb098809679250d5a19
Nina <128163423+ninakrivulets@users.noreply.github.com>
2025-04-23 11:08:03 +0300

    Add files via upload

--- spytrometer/source/generate_training_data.py
+++ spytrometer/source/generate_training_data.py
@@ -5,7 +5,7 @@ import pyopenms as oms
 import logging
 import uuid
 import base64
-
+from Bio import SeqIO
 import shutil
 
 # folder for saving
@@ -84,8 +84,7 @@ def parse_protein_id(protein_id_str, seq_len):
             print(f"Error parsing protein ID {protein}: {e}")
     return protein_dict
 
-def save_partial_data(data, file_counter, plogger):
-    output_file = os.path.join(output_folder, f"PXD028806_tailor_{file_counter}.pkl")
+def save_partial_data(data, output_file, plogger):
     try:
         with open(output_file, "wb") as f:
             data = pd.DataFrame(data)
@@ -94,7 +93,17 @@ def save_partial_data(data, file_counter, plogger):
         plogger.info(f"Saved {len(data)} rows to {output_file}") 
     except Exception as e:
         print(f"Error while saving to {output_file}: {e}")
-        plogger.info(f"Error while saving to {output_file}: {e}") 
+        plogger.info(f"Error while saving to {output_file}: {e}")  
+
+def save_dict_to_pickle(protein_to_sequences, output_file, plogger):
+    try:
+        with open(output_file, "wb") as f:
+            pickle.dump(protein_to_sequences, output_file)
+        print(f"Saved dixtionary to {output_file}")
+        plogger.info(f"Saved dictionary to {output_file}") 
+    except Exception as e:
+        print(f"Error while saving to {output_file}: {e}")
+        plogger.info(f"Error while saving to {output_file}: {e}")  
 
 
 def generate_uuid():
@@ -127,7 +136,7 @@ def get_mz_and_intensities(df, spectra_per_file=1000):
             exp = oms.MSExperiment()
             oms.MzMLFile().load(mzml_file, exp)
             spectra = exp.getSpectra()
-            
+            protein_to_sequences = {}
             for spec in spectra:
                 #plogger.info(f"still working for {spec}") 
                 native_id = spec.getNativeID()
@@ -168,11 +177,29 @@ def get_mz_and_intensities(df, spectra_per_file=1000):
                             'sequence': sequence,
                             'protein_ids': protein_dict
                         })
+                        
+                        if sequence and isinstance(protein_dict, dict):
+                            for protein, positions in protein_dict.items():
+                                if protein not in protein_to_sequences:
+                                    protein_to_sequences[protein] = []
+                                protein_to_sequences[protein].append(sequence)
+
                         processed_spectra += 1
                         
                         # Save after every `spectra_per_file` rows
+                        
                         if len(results) >= spectra_per_file:
-                            save_partial_data(results, file_counter, plogger)
+                            output_file = os.path.join(output_folder, f"PXD028806_tailor_{file_counter}.pkl")
+                            save_partial_data(results, output_file, plogger)
+                            protein_collector = ProteinCollection()
+
+                            path_to_fasta = "/home/data/Fasta/uniprot-proteome_UP000005640+reviewed_yes.fasta"
+
+                            # loading data from fasta file
+                            protein_collector.load_fasta(path_to_fasta)
+                            save_dict_to_pickle(protein_to_sequences, output_file, plogger)
+                            #pickle_file_path = "/home/data/Fasta/uniprot-proteome_UP000005640+reviewed_yes.pkl"
+                            protein_collector.save_to_pickle(output_file)
                             file_counter += 1
                             results = []  # Clear results for next batch
 
@@ -183,10 +210,50 @@ def get_mz_and_intensities(df, spectra_per_file=1000):
     
     # Save remaining results
     if results:
-        save_partial_data(results, file_counter, plogger)
+        output_file = os.path.join(output_folder, f"PXD028806_tailor_{file_counter}.pkl")
+        save_partial_data(results, output_file, plogger)
     plogger.info(f"Processed {processed_spectra} spectra in total.")
     print(f"Processed {processed_spectra} spectra in total.")
 
-get_mz_and_intensities(df, spectra_per_file=1000)
 
+# defining class ProteinObj
+class ProteinObj:
+    def __init__(self, index, protein_id, sequence):
+        self.index = index
+        self.protein_id = protein_id
+        self.sequence = sequence
+
+    def __repr__(self):
+        return f"ProteinObj({self.index}, {self.protein_id}, {self.sequence[:10]}...)"  
+    
+# Class for working with a collection
+class ProteinCollection:
+    def __init__(self):
+        self.protein_collection = []
+        self.long_sequence = ""  # String for merged sequence
+        self.position_dict = {}  # Dictionary for start positions
+
+    def load_fasta(self, path_to_fasta):
+        cnt = 0
+        for record in SeqIO.parse(path_to_fasta, "fasta"):
+            # add protein
+            self.protein_collection.append(
+                ProteinObj(cnt, str(record.id), str(record.seq))
+            )
+            # adding protein to the sequence
+            self.long_sequence += str(record.seq)
+            # saving the starting position
+            self.position_dict[str(record.id)] = len(self.long_sequence) - len(str(record.seq))
+            cnt += 1
+            # print('Finished with', cnt)
+
+    def save_to_pickle(self, file_path):
+        # saving the merged sequence and the dict to pickle file
+        with open(file_path, 'wb') as f:
+            pickle.dump({'long_sequence': self.long_sequence, 'position_dict': self.position_dict}, f)
+
+    def __repr__(self):
+        return f"ProteinCollection with {len(self.protein_collection)} proteins"
+
+get_mz_and_intensities(df, spectra_per_file=10000)
 
